4a422455766a517f2c92506011aeeb5c67e6bff1
nienzu <ibqqz0602@gmail.com>
2020-03-31 18:21:46 +0800

    新增安裝相依套件的腳本

---
+++ install_build_require_ubuntu.sh
@@ -0,0 +1,11 @@
+sudo apt update -y
+# Install for oxool-community
+sudo apt install -y build-essential libsqlite3-dev libcurl4-openssl-dev libcppunit-dev libcap-dev libtool libpng-dev automake m4 wget curl autoconf pkg-config openssl
+# Install for build deb package
+sudo apt install -y devscripts debhelper dh-systemd
+# Install for poco
+sudo apt install -y libodbc1 libpcre16-3 libpcre3-dev libpcre32-3 libpcrecpp0v5
+# Install for oxoffice
+sudo apt install -y openjdk-8-jdk libgumbo1 hunspell
+sudo apt install -y python-pip
+sudo pip install polib
