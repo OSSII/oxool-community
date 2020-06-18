a1b6df3519db693979038b9c9ffe537f8e40e0bb
Firefly <firefly@ossii.com.tw>
2020-06-18 17:01:18 +0800

    Encode with encodeURI before executing the macro to avoid blank spaces being cut off.

--- loleaflet/src/control/Macro.js
+++ loleaflet/src/control/Macro.js
@@ -48,6 +48,6 @@ L.Map.include({
 			this._macroCallbacks[macroCmd] = callback;
 		}
 
-		this._socket.sendMessage('runmacro ' + command);
+		this._socket.sendMessage('uno ' + encodeURI(command));
 	},
 });
