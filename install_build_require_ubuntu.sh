9c66ffec7bc0c71e7a2b83b67aaea5d59b03840b
nienzu <ibqqz0602@gmail.com>
2020-08-02 01:37:58 +0800

    Update ubuntu installation script

--- install_build_require_ubuntu.sh
+++ install_build_require_ubuntu.sh
@@ -1,8 +1,8 @@
 sudo apt update -y
 # Install for oxool-community
-sudo apt install -y build-essential libsqlite3-dev libcurl4-openssl-dev libcppunit-dev libcap-dev libtool libpng-dev automake m4 wget curl autoconf pkg-config openssl net-tools
+sudo apt install -y build-essential libsqlite3-dev libcurl4-openssl-dev libcppunit-dev libcap-dev libtool libpng-dev automake m4 wget curl autoconf pkg-config openssl net-tools libgumbo-dev ccache
 # Install for build deb package
-sudo apt install -y devscripts debhelper dh-systemd
+sudo apt install -y devscripts debhelper dh-systemd dh-exec
 # Install for poco
 sudo apt install -y libodbc1 libpcre16-3 libpcre3-dev libpcre32-3 libpcrecpp0v5
 # Install for oxoffice
