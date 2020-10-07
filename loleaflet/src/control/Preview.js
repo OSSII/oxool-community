d2f48282aad84f611a84341d937b19316b85b2b5
Firefly <firefly@ossii.com.tw>
2020-10-07 10:34:06 +0800

    calc: Spreadsheet preview zoom can be from 60%~200%.

--- loleaflet/src/control/Preview.js
+++ loleaflet/src/control/Preview.js
@@ -120,10 +120,13 @@ L.Control.Preview = L.Control.extend({
 		});
 
 		var viewerbar = w2ui['viewerbar'];
-		// 試算表的縮放比例不能低於 100%
+		// 試算表的縮放比例不能低於 60%
 		if (docType === 'spreadsheet') {
 			viewerbar.set('zoom', {
 				items: [
+					{ id: 'zoom60', text: '60%', scale: 7},
+					{ id: 'zoom70', text: '70%', scale: 8},
+					{ id: 'zoom85', text: '85%', scale: 9},
 					{ id: 'zoom100', text: '100%', scale: 10},
 					{ id: 'zoom120', text: '120%', scale: 11},
 					{ id: 'zoom150', text: '150%', scale: 12},
