43b50533f3575e3d8cc1f71a9e00ab5acd1e911e
Firefly <firefly@ossii.com.tw>
2022-11-16 15:39:17 +0800

    admin: Added main menu independent profile.

---
+++ loleaflet/admin/js/admin.menu.js
@@ -0,0 +1,22 @@
+var mainMenus = [
+	{
+		file: 'admin.html',
+		icon: 'info-circle',
+		name: _('Overview')
+	},
+	{
+		file: 'adminAnalytics.html',
+		icon: 'graph-up',
+		name: _('Analytics')
+	},
+	{
+		file: 'adminLog.html',
+		icon: 'journal-text',
+		name: _('Log')
+	},
+	{
+		file: 'adminFontManager.html',
+		icon: 'file-earmark-font',
+		name: _('Font manager')
+	},
+];
