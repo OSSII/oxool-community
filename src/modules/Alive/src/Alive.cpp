15ce1f67a3e5bc8c30b47a572eca80786dc22dcb
Firefly <firefly@ossii.com.tw>
2023-03-04 23:41:20 +0800

    Alive.cpp : Add license statement header.

--- src/modules/Alive/src/Alive.cpp
+++ src/modules/Alive/src/Alive.cpp
@@ -1,6 +1,14 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 #include <OxOOL/HttpHelper.h>
 #include <OxOOL/Module/Base.h>
+
+#include <net/Socket.hpp>
 
 class Alive : public OxOOL::Module::Base
 {
@@ -14,3 +22,5 @@ public:
 };
 
 OXOOL_MODULE_EXPORT(Alive);
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
