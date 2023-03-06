aa565d3f7f0c0c79333be30f3d47cfc3a62fcec4
Firefly <firefly@ossii.com.tw>
2023-03-06 12:16:05 +0800

    Added license header.

--- src/lib/OxOOL/Module/Base.cpp
+++ src/lib/OxOOL/Module/Base.cpp
@@ -1,3 +1,9 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 #include <config.h>
 #include <memory>
@@ -296,3 +302,5 @@ void Base::preprocessAdminFile(const std::string& adminFile,
 
 } // namespace Module
 } // namespace OxOOL
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
