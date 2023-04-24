CollaboraOnline/online

Improve comment formatting on mobile
Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I0dc58bba96edec320f771ef704be31acbf3082b4

browser/src/layer/tile/CommentListSection.ts
@@ -392,7 +392,7 @@ class CommentSection {
 			contentClassName: 'vex-has-inputs',
 			message: '',
 			input: [
-				'<textarea name="comment" id="new-mobile-comment-input-area" class="cool-annotation-textarea" required>' + (commentData.text && isMod ? commentData.text: '') + '</textarea>'
+				'<textarea name="comment" id="new-mobile-comment-input-area" class="cool-annotation-textarea" required></textarea>'
 			].join(''),
 			buttons: [
 				$.extend({}, vex.dialog.buttons.YES, { text: _('Save') }),
@@ -451,6 +451,7 @@ class CommentSection {
 
 		dialog.contentEl.insertBefore(author, dialog.contentEl.childNodes[0]);
 
+		$(dialog.contentEl).find('textarea').text((commentData.text && isMod ? commentData.text : ''));
 		$(dialog.contentEl).find('textarea').focus();
 	}
 

OSSII/oxool-community

loleaflet/src/layer/tile/CommentListSection.ts
@@ -392,7 +392,7 @@ class CommentSection {
 			contentClassName: 'vex-has-inputs',
 			message: '',
 			input: [
-				'<textarea name="comment" id="new-mobile-comment-input-area" class="oxool-annotation-textarea" required>' + (commentData.text && isMod ? commentData.text: '') + '</textarea>'
+				'<textarea name="comment" id="new-mobile-comment-input-area" class="oxool-annotation-textarea" required></textarea>'
 			].join(''),
 			buttons: [
 				$.extend({}, vex.dialog.buttons.YES, { text: _('Save') }),
@@ -451,6 +451,7 @@ class CommentSection {
 
 		dialog.contentEl.insertBefore(author, dialog.contentEl.childNodes[0]);
 
+		$(dialog.contentEl).find('textarea').text((commentData.text && isMod ? commentData.text : ''));
 		$(dialog.contentEl).find('textarea').focus();
 	}
 